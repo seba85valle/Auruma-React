a06624d46811f42ff3d4e40a6d487f91d92bff7e
seba85valle <seba85valle@gmail.com>
2025-10-16 18:54:11 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'   // 👈 agregar
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename={import.meta.env.BASE_URL}>  
+  <StrictMode>
     <App />
-  </BrowserRouter>
+  </StrictMode>,
 )
