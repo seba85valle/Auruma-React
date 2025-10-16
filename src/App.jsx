ea57ef4bf34bbc6d3f9348f0ec7542606b986779
seba85valle <seba85valle@gmail.com>
2025-10-16 16:30:01 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import "./App.css";
 function App() {
   return (
     <CartProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Header />
         <Nav />
         <Routes>
