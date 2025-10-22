0d483102457e04ae013303274bb0092879add4af
seba85valle <seba85valle@gmail.com>
2025-10-22 16:02:35 -0300

    Update Item.jsx

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,12 +1,13 @@
 import "./Item.css";
 
-export const Item = ({ name, price, description, imageUrl, children }) => {
+export const Item = ({ name, price, description, imageUrl, size, children }) => {
   return (
     <div className="card h-100 shadow-sm producto-card text-center">
       <img src={imageUrl} alt={name} className="card-img-top producto-img" />
       <div className="card-body d-flex flex-column justify-content-between">
         <div>
           <h5 className="card-title">{name}</h5>
+          {size && <p className="card-size">{size}</p>}
           <p className="card-text">{description}</p>
         </div>
         <p className="fw-bold precio">${price.toLocaleString()}</p>
@@ -15,3 +16,4 @@ export const Item = ({ name, price, description, imageUrl, children }) => {
     </div>
   );
 };
+
