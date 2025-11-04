9b72f2cbc2b786984e46f3bfa9853389bb25d55d
seba85valle <seba85valle@gmail.com>
2025-11-04 19:27:35 -0300

    Update products.js

--- src/services/products.js
+++ src/services/products.js
@@ -1,1 +1,14 @@
+const BASE_URL = "https://6908eeba2d902d0651b22ef5.mockapi.io/products";
 
+export const createProduct = async (product) => {
+    const res = await fetch(BASE_URL, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(product)
+    });
+    if(!res.ok) {
+        throw new Error("No se pudo crear el producto");
+    }
+    const result = await res.json();
+    return result;
+};
