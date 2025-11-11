31b1790879377b15ea3f0366c30d94b7940a9b67
seba85valle <seba85valle@gmail.com>
2025-11-11 18:35:19 -0300

    Update MainLayout.jsx

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,1 +1,13 @@
+import {Outlet} from 'react-router-dom';
+import {Nav} from "../components/Nav,Nav";
 
+export const MainLayout = () => {
+    return (
+        <>
+            <Nav />
+            <div>
+                <Outlet />
+            </div>
+        </>
+    );
+};
